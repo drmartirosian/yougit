3b7c3e9275853836a0a94e6b2aa6783fcb7acfe1
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-27 03:52:13 -0600

    Card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
-// import logo from './logo.svg';
-import { Form } from 'semantic-ui-react'
+import { Form, Card, Image, Icon } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
@@ -16,14 +15,34 @@ function App() {
   return (
     <div>
       <div className="navbar"> Github search </div>
-      <div classname="search">
-        <Form>
-          <Form.Group>
-            <Form.Input placeholder='Name' name='name' />
-            <Form.Button content='Submit'/>
-          </Form.Group>
-        </Form>
-      </div>
+        <div className="search">
+          <Form>
+            <Form.Group>
+              <Form.Input placeholder='Github user' name='github user' />
+              <Form.Button content='search'/>
+            </Form.Group>
+          </Form>
+        </div>
+        <div className="card">
+          <Card>
+          <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              22 Friends
+            </a>
+          </Card.Content>
+          </Card>
+        </div>
     </div>
   );
 }
