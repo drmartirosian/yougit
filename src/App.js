c2d5c222e8184ad5718029c689ab3bcc12cddac0
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-27 04:06:07 -0600

    Fetching Github API data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Form, Card, Image, Icon } from 'semantic-ui-react'
+import { Form, Card, Icon, Image } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
@@ -12,8 +12,30 @@ function App() {
   const [userInput, setUserInput] = useState('');
   const [error, setError] = useState('');
 
+  //GET USER DATA
+  useEffect(()=>{
+    fetch("https://api.github.com/users/example")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setData(data)
+    })
+  }, []);
+
+  //Set state
+  const setData = ({ name, login, followers, following, public_repos, avatar_url }) => {
+    setUserName(name)
+    setUserName(login)
+    setFollowers(followers)
+    setFollowing(following)
+    setRepos(public_repos)
+    setAvatar(avatar_url)
+  }
+
+
   return (
     <div>
+
       <div className="navbar"> Github search </div>
         <div className="search">
           <Form>
@@ -23,26 +45,21 @@ function App() {
             </Form.Group>
           </Form>
         </div>
+
         <div className="card">
           <Card>
-          <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>Matthew</Card.Header>
-            <Card.Meta>
-              <span className='date'>Joined in 2015</span>
-            </Card.Meta>
-              <Card.Description>
-                Matthew is a musician living in Nashville.
-              </Card.Description>
+            <Image src='https://avatars1.githubusercontent.com/u/57936?v=4' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta> <span className='date'>Joined in 2015</span> </Card.Meta>
+                <Card.Description> Matthew is a musician living in Nashville. </Card.Description>
+              </Card.Content>
+            <Card.Content extra>
+              <a> <Icon name='user' /> 22 Friends </a>
             </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Icon name='user' />
-              22 Friends
-            </a>
-          </Card.Content>
           </Card>
         </div>
+
     </div>
   );
 }
